83fe3d28c4a36ebf3e641b0e3ec51905764dc8c4
Taly <trabinerson@paypal.com>
2015-10-19 16:06:54 +0300

    Added spinner while similar movies are loading

--- app/src/main/java/com/example/trabinerson/moviefiend/DetailsActivity.java
+++ app/src/main/java/com/example/trabinerson/moviefiend/DetailsActivity.java
@@ -1,12 +1,14 @@
 package com.example.trabinerson.moviefiend;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
@@ -23,11 +25,14 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     private static final String LOG_TAG = DetailsActivity.class.getSimpleName();
 
     private int mMovieId;
+    private SimilarMoviesHolder mSimilarMoviesHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_details);
+
+        // Get holder
 
         // Get movie
         Intent intent = getIntent();
@@ -35,20 +40,10 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
         mMovieId = movie.getId();
         Log.i(LOG_TAG, "Unbundled " + movie.getName());
 
-        // Get views
+        // Set movie on screen
         View rootView = findViewById(R.id.details_root);
-        NetworkImageView posterView = (NetworkImageView) rootView.findViewById(R.id.imageview_details_poster);
-        TextView nameView = (TextView) rootView.findViewById(R.id.textview_movie_name);
-        TextView ratingView = (TextView) rootView.findViewById(R.id.textview_movie_rating);
-        TextView descriptionView = (TextView) rootView.findViewById(R.id.textview_movie_description);
-
-        // Set data
-        ImageLoader imageLoader = RequestQueueSingleton.getInstance().getImageLoader();
-        nameView.setText(movie.getName());
-        String rating = getString(R.string.rating, movie.getRating());
-        ratingView.setText(rating);
-        posterView.setImageUrl(movie.getPosterUrl(), imageLoader);
-        descriptionView.setText(movie.getDescription());
+        mSimilarMoviesHolder = new SimilarMoviesHolder(rootView);
+        mSimilarMoviesHolder.setMovie(this, movie);
 
         // Init loader
         getLoaderManager().initLoader(LOADER_ID, null, this);
@@ -62,11 +57,44 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager.
     @Override
     public void onLoadFinished(Loader<Movie[]> loader, Movie[] data) {
         Log.i(LOG_TAG, "Got " + data.length + " similar movies");
-        // TODO
+        mSimilarMoviesHolder.finishLoading();
     }
 
     @Override
     public void onLoaderReset(Loader<Movie[]> loader) {
         // TODO (?)
+    }
+
+    public static class SimilarMoviesHolder {
+
+        private final NetworkImageView mPosterView;
+        private final TextView mNameView;
+        private final TextView mRatingView;
+        private final TextView mDescriptionView;
+        private final TextView mSimilarMoviesText;
+        private final ProgressBar mProgressBar;
+
+        public SimilarMoviesHolder(View rootView) {
+            this.mPosterView = (NetworkImageView) rootView.findViewById(R.id.imageview_details_poster);
+            this.mNameView = (TextView) rootView.findViewById(R.id.textview_movie_name);
+            this.mRatingView = (TextView) rootView.findViewById(R.id.textview_movie_rating);
+            this.mDescriptionView = (TextView) rootView.findViewById(R.id.textview_movie_description);
+            this.mSimilarMoviesText = (TextView) rootView.findViewById(R.id.textview_similar_movies);
+            this.mProgressBar = (ProgressBar) rootView.findViewById(R.id.progress_bar_similar_movies);
+        }
+
+        public void setMovie(Context context, Movie movie) {
+            ImageLoader imageLoader = RequestQueueSingleton.getInstance().getImageLoader();
+            this.mNameView.setText(movie.getName());
+            String rating = context.getString(R.string.rating, movie.getRating());
+            this.mRatingView.setText(rating);
+            this.mPosterView.setImageUrl(movie.getPosterUrl(), imageLoader);
+            this.mDescriptionView.setText(movie.getDescription());
+        }
+
+        public void finishLoading() {
+            this.mProgressBar.setVisibility(View.GONE);
+            this.mSimilarMoviesText.setVisibility(View.VISIBLE);
+        }
     }
 }
