6bf890ac5e475d63f1cfcc160d7e89b93b2f9424
Taly <trabinerson@paypal.com>
2015-10-25 09:48:40 +0200

    Added old loaders folder in correct case

---
+++ app/src/main/java/com/example/trabinerson/moviefiend/loaders/SimilarMoviesLoader.java
@@ -0,0 +1,28 @@
+package com.example.trabinerson.moviefiend.loaders;
+
+import android.content.Context;
+import android.net.Uri;
+
+/**
+ * A loader for the list of similar movies of a given movie.
+ */
+public class SimilarMoviesLoader extends MoviesLoader {
+
+    private static final String SIMILAR_MOVIES_ENDPOINT = "similar";
+
+    private int mMovieId;
+
+    public SimilarMoviesLoader(Context context, int movieId) {
+        super(context);
+        mMovieId = movieId;
+    }
+
+    @Override
+    protected Uri.Builder getBaseUriBuilder() {
+        String movieId = Integer.toString(mMovieId);
+        Uri.Builder builder = Uri.parse(MOVIE_API_BASE_URL).buildUpon()
+                .appendPath(movieId)
+                .appendPath(SIMILAR_MOVIES_ENDPOINT);
+        return builder;
+    }
+}
