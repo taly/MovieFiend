7769028636995d00df5fd645db0fc60975a73a39
Taly <trabinerson@paypal.com>
2015-10-27 16:02:10 +0200

    Removed catch clause for ClassCastException

--- app/src/main/java/com/example/trabinerson/moviefiend/fragments/InTheatresFragment.java
+++ app/src/main/java/com/example/trabinerson/moviefiend/fragments/InTheatresFragment.java
@@ -35,12 +35,7 @@ public class InTheatresFragment extends Fragment
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        try {
-            mCallback = (InTheatresCallbacks) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement InTheatresCallbacks");
-        }
+        mCallback = (InTheatresCallbacks) context;
     }
 
     @Override
--- app/src/main/java/com/example/trabinerson/moviefiend/fragments/MovieDetailsFragment.java
+++ app/src/main/java/com/example/trabinerson/moviefiend/fragments/MovieDetailsFragment.java
@@ -66,12 +66,7 @@ public class MovieDetailsFragment extends Fragment
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        try {
-            mCallback = (MovieDetailsCallbacks) context;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(context.toString()
-                    + " must implement MovieDetailsCallbacks");
-        }
+        mCallback = (MovieDetailsCallbacks) context;
     }
 
     @Override
